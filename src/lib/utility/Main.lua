bf5b56948eed517068ab1f79685fbe5f8288c5ef
TyKonKet <matteoautieri@live.it>
2021-01-27 11:07:38 +0100

    utility update

--- src/lib/utility/Main.lua
+++ src/lib/utility/Main.lua
@@ -2,13 +2,19 @@
 -- Royal Utility
 --
 -- @author Royal Modding
--- @version 1.5.1.0
+-- @version 1.7.1.0
 -- @date 21/11/2020
 
 --- Initialize RoyalUtility
 ---@param utilityDirectory string
 function InitRoyalUtility(utilityDirectory)
     source(Utils.getFilename("Utility.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityDebug.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityEntity.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityGameplay.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityString.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityTable.lua", utilityDirectory))
+    source(Utils.getFilename("UtilityInterpolator.lua", utilityDirectory))
     g_logManager:devInfo("Royal Utility loaded successfully by " .. g_currentModName)
     return true
 end
